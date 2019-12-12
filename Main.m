c12c92a6ee7b80d07a00ea3bf8e2091bf1e25bb7
udaka21 <udaka21@gmail.com>
2019-12-12 21:40:11 +0900

    Add writing function for the processed Images

--- Main.m
+++ Main.m
@@ -1,6 +1,11 @@
 %read the images from the folder
-Image=imread('Images/170057.jpg');
-ImageHuman=imread('Images/170057_human.jpg');
+path = 'Images/';
+imName = '170057.jpg';
+imHumanName = '170057_human.jpg';
+imNumber = extractBefore(imName, 7);
+
+Image=imread(fullfile (path,imName));
+ImageHuman=imread(fullfile (path,imHumanName));
 ImageGray=rgb2gray(Image);
 %Imagegray=rgb2gray(ImageHuman);
 Real=edge(ImageGray,'sobel', .04);
@@ -9,7 +14,7 @@ Real=edge(ImageGray,'sobel', .04);
 c1 = imadjust(Image, [0 0 0;0.2 1 1], [0 0 0;1 1 1],1);
 c2 = imadjust(Image, [0 0 0;1 0.2 1], [0 0 0;1 1 1],1);
 c3=rgb2gray(c2);
-c11=edge(c3,'sobel', .06);
+color=edge(c3,'sobel', .06);
 
 %Brightness greadient
 g1=imadjust(Image, [0 1], [0 1],2);
@@ -17,26 +22,42 @@ g3=rgb2gray (g1);
 g2=imadjust(Image, [0 1], [0 1],0.6);
 g4=rgb2gray(g2);
 I5=edge(g3,'sobel', .06);
-I6=edge(g4,'sobel', .06);
+brightness=edge(g4,'sobel', .06);
+
+
 
 %K-means
 BW3=imsegkmeans(Image,2,'NumAttempts',10); 
-BW7=edge(BW3,'sobel') ;
+kmeans=edge(BW3,'sobel') ;
 
 %MeanShift
-bw=0.02;                    % Mean Shift Bandwidth
+bw=0.06;                    % Mean Shift Bandwidth
 [Ims, Nms]=Ms(Image,bw);    % Mean Shift (color)
 Ims1=rgb2gray(Ims);
-Ims2=edge(Ims1,'sobel') ;
+meanshift=edge(Ims1,'sobel') ;
 
-% imshow(BW7);title('K-Means');
+%create 
+str1=strcat(path,'Results/',imNumber,'_real','.jpg');
+str2=strcat(path,'Results/',imNumber,'_color','.jpg');
+str3=strcat(path,'Results/',imNumber,'_brightness','.jpg');
+str4=strcat(path,'Results/',imNumber,'_kmeans','.jpg');
+str5=strcat(path,'Results/',imNumber,'_meanshift','.jpg');
+
+%Writing data in to 'Results' Folder
+imwrite(Real,str1);
+imwrite(color,str2);
+imwrite(brightness,str3);
+imwrite(kmeans,str4);
+imwrite(meanshift,str5);
+
+
 
 %Show the Image
 subplot(151);imshow(Real);%  title('Original');
-subplot(152);imshow(c11); % title('Color');
-subplot(153);imshow(I6); % title('Brigthness');
-subplot(154);imshow(BW7); % title('K-Means');
-subplot(155);imshow(Ims2); % title('Mean Shift');
+subplot(152);imshow(color); % title('Color');
+subplot(153);imshow(brightness); % title('Brigthness');
+subplot(154);imshow(kmeans); % title('K-Means');
+subplot(155);imshow(meanshift); % title('Mean Shift');
 
 
 
