c7a30a63ad6765b37782ac63c8c466f7981351f6
udaka21 <udaka21@gmail.com>
2019-12-12 20:10:27 +0900

    Corrected Runtime errors

--- Main.m
+++ Main.m
@@ -10,7 +10,7 @@ c1 = imadjust(Image, [0 0 0;0.2 1 1], [0 0 0;1 1 1],1);
 c2 = imadjust(Image, [0 0 0;1 0.2 1], [0 0 0;1 1 1],1);
 c3=rgb2gray(c2);
 c11=edge(c3,'sobel', .06);
-% 
+
 %Brightness greadient
 g1=imadjust(Image, [0 1], [0 1],2);
 g3=rgb2gray (g1);
@@ -20,11 +20,11 @@ I5=edge(g3,'sobel', .06);
 I6=edge(g4,'sobel', .06);
 
 %K-means
-% BW3=imsegkmeans(Image,2,'NumAttempts',10); 
-% BW7=edge(BW3,'sobel') ;
+BW3=imsegkmeans(Image,2,'NumAttempts',10); 
+BW7=edge(BW3,'sobel') ;
 
 %MeanShift
-bw=0.09;                     % Mean Shift Bandwidth
+bw=0.02;                    % Mean Shift Bandwidth
 [Ims, Nms]=Ms(Image,bw);    % Mean Shift (color)
 Ims1=rgb2gray(Ims);
 Ims2=edge(Ims1,'sobel') ;
@@ -32,16 +32,11 @@ Ims2=edge(Ims1,'sobel') ;
 % imshow(BW7);title('K-Means');
 
 %Show the Image
-% subplot(151);imshow(Real);%  title('Original');
-% subplot(152);imshow(c11); % title('Color');
-% subplot(153);imshow(I6); % title('Brigthness');
-% subplot(154);imshow(BW7); % title('K-Means');
-% subplot(155);
-imshow(Ims2); % title('Mean Shift');
+subplot(151);imshow(Real);%  title('Original');
+subplot(152);imshow(c11); % title('Color');
+subplot(153);imshow(I6); % title('Brigthness');
+subplot(154);imshow(BW7); % title('K-Means');
+subplot(155);imshow(Ims2); % title('Mean Shift');
 
-%multi = cat(Real,c11,I6,BW7,Ims2);
-% Download 1 by one and mearge
-%montage(multi,[1 5]);
 
-%abc = corr2(Real,BW7);
 
